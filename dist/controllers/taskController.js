aebc8857731ee05feff706078eb13c899fa72679
SubhamReddddy <subhamreddy121@gmail.com>
2025-03-01 16:52:01 +0530

    sending res from addtask api

--- dist/controllers/taskController.js
+++ dist/controllers/taskController.js
@@ -4,7 +4,9 @@ import ErrorHandler from "../utils/ErrorHandler.js";
 export const addtask = catchAsync(async (req, res, next) => {
     const { data, result } = req.body;
     const task = await taskModel.findOneAndUpdate({ userID: req.user?._id }, { $push: { carrier: { task: data, ans: result } } }, { upsert: true, new: true, setDefaultsOnInsert: true });
-    res.status(200).end();
+    res.status(200).json({
+        task,
+    });
 });
 export const getalltask = catchAsync(async (req, res, next) => {
     const tasks = await taskModel.findOne({ userID: req.user?._id });
